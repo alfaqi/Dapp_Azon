fb8211a4c6782c0be17c3affd05e957c3970c6a9
alfaqi <alfaqiabdullah@gmail.com>
2023-05-01 16:34:25 +0300

    Edit the contract the test code

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
-    address public immutable i_onwer;
+    address public immutable i_owner;
     struct Item {
         uint256 id;
         string name;
@@ -13,10 +13,24 @@ contract Dappazon {
         uint256 stock;
     }
 
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == i_owner);
+        _;
+    }
 
     constructor() {
-        i_onwer = msg.sender;
+        i_owner = msg.sender;
     }
 
     // list products
@@ -28,7 +42,8 @@ contract Dappazon {
         uint256 _cost,
         uint256 _rating,
         uint256 _stock
-    ) public {
+    ) public onlyOwner {
+        //Create item struct
         Item memory item = Item(
             _id,
             _name,
@@ -38,10 +53,34 @@ contract Dappazon {
             _rating,
             _stock
         );
+
+        // Save Item struct to blockchain
         items[_id] = item;
+
+        //Emit event
+        emit List(_name, _cost, _stock);
     }
 
     // buy products
+    function buy(uint _id) public payable {
+        // Recive Crypto
+        // --> Make the function payable
+
+        // Fetch item
+        Item memory item = items[_id];
+
+        // Create an order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order to user
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Substrack stock
+        items[_id].stock = item.stock - 1;
+
+        // Emit event
+    }
 
     // withdraw funds
 }
--- test/dappazon.js
+++ test/dappazon.js
@@ -21,8 +21,8 @@ describe('Dappazon', () => {
     })
 
     describe('Deployment', () => {
-        it('Set the onwer', async () => {
-            expect(await dappazon.i_onwer()).to.equal(deployer.address)
+        it('Set the owner', async () => {
+            expect(await dappazon.i_owner()).to.equal(deployer.address)
         })
     })
 
@@ -43,7 +43,7 @@ describe('Dappazon', () => {
 
         it('Returns item', async () => {
             const item = await dappazon.items(ID);
-            
+
             expect(item.id).to.equal(ID)
             expect(item.name).to.equal(NAME)
             expect(item.category).to.equal(CATEGORY)
@@ -51,9 +51,49 @@ describe('Dappazon', () => {
             expect(item.cost).to.equal(COST)
             expect(item.rating).to.equal(RATING)
             expect(item.stock).to.equal(STOCK)
+        })
 
+        it("Emit List event", async () => {
+            await expect(transaction).to.emit(dappazon, "List")
         })
 
     })
+    describe('Buying', () => {
+        let transaction
+        beforeEach(async () => {
+            transaction = await dappazon.connect(deployer).list(
+                ID,
+                NAME,
+                CATEGORY,
+                IMAGE,
+                COST,
+                RATING,
+                STOCK
+            )
+            await transaction.wait()
 
+            transcation = await dappazon.connect(buyer).buy(ID, { value: COST })
+        })
+
+        it("Updates buyers's order count", async () => {
+            const result = await dappazon.orderCount(buyer.address)
+            expect(result).to.equal(1);
+        })
+
+        it("Adds the order", async () => {
+            const order = await dappazon.orders(buyer.address, 1);
+
+            expect(order.time).to.be.greaterThan(0);
+            expect(order.item.name).to.equal(NAME);
+        })
+
+        it("Updates the contract balance", async () => {
+            const result = await ethers.provider.getBalance(dappazon.address);
+            expect(result).to.equal(COST);
+        })
+
+        it("Emits buy event", async () => {
+            await expect(transaction).to.emit(dappazon, "Buy");
+        })
+    })
 })
